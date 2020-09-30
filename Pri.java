a1958cfc81bcf1ba9110ddc655b292b55a768d42
iayushguptaaa <57136524+iayushguptaaa@users.noreply.github.com>
2020-10-01 02:21:16 +0530

    Delete Pri.java

--- Pri.java
+++
@@ -1,9 +0,0 @@
-public class Pri 
-{
-    public static void main(String[] args) 
-    {
-        System.out.print("Y" + "O");
-        System.out.print('L');
-        System.out.print('O');
-    }
-}
